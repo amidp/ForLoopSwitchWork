cd4af8568c108a63d70a33740b62bdd58e134212
amidh <amidp18@gmail.com>
2021-10-30 21:13:47 +0100

    first task completed

--- src/PrintNos.java
+++ src/PrintNos.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class PrintNos {
     public static void main(String[] args) {
-        System.out.println(" 12");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter number between 23-33");
+        System.out.println(" Enter the starting number : ");
+        int a1 = sc.nextInt();
+        System.out.println(" Enter the exit number : ");
+        int a2 = sc.nextInt();
+        for (int i = a1; i <= a2; i++) {
+            System.out.println(i);
+        }
     }
 }
